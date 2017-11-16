7b076df886b50d3873f94b60ccd5fa0702830fc2
scinetic <scinetic@gmail.com>
2017-11-16 18:48:56 +0100

    example commit

---
+++ Kaloyan/index.js
@@ -0,0 +1,11 @@
+module.exports = function() {
+  
+  let exercise = function() {
+
+    return { "Kaloyan": "wtf is this" }
+  }
+  
+  let value = exercise();
+  let result = {"myValue": value};
+  return result;
+}
